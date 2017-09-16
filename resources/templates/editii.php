b8994d7fda19c947e129e7a1d8c924103084e754
cristan <grigorecristian@gmail.com>
2017-09-16 11:33:38 +0300

    Broken table

--- resources/templates/editii.php
+++ resources/templates/editii.php
@@ -1,47 +1,82 @@
 <?php
 
 $revistaId = $_POST["reviste"];
-$toateEditiile = $db->query("SELECT * FROM editii WHERE revista_id = '$revistaId'; ");
+$toateEditiile = $db->query("SELECT * FROM editii WHERE revista_id = '$revistaId' AND tip = 'revista'; ");
 
 afiseazaTabel($toateEditiile);
 
-// TODO fix this
+//afiseazaMotherFuckingTabelCaCelaltSeIntrerupe($toateEditiile);
+
+
+// FIXME: WTF echo nu afiseaza tot outputul
 function afiseazaTabel($toateEditiile) {
     $randuri = getRows($toateEditiile);
-    echo <<<_BAGAHTML
+
+    $output = "" .
+    "<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset=\"utf-8\">
+    </head>
+    <body>
 	<table>
-	    <thead>
 	        <tr>
 	            <th>An</th>
 	            <th>Luna</th>
 	            <th>Nr.</th>
 	            <th>Joc full</th>
-	        </tr>
-	    </thead>
-	    <tbody>
-	        $randuri;
-	    </tbody>
-    </table>
-_BAGAHTML;
+	        </tr> "
+            . $randuri
+    . "</table>
+    </body>
+    </html>";
+
+    echo $output;
 }
 
 function getRows($toateEditiile) {
     $randuri = "";
     while ($row = $toateEditiile->fetchArray(SQLITE3_ASSOC)) {
+        $id = $row['editie_id'];
         $an = $row['an'];
         $luna = $row['luna'];
         $nr = $row['numar'];
         $joc_complet = $row['joc_complet'];
-        $randuri .= getRowTableData($an, $luna, $nr, $joc_complet);
-//        echo $an . " | " . $luna . " | " . $nr . " | " . $joc_complet;
+        $randuri .= getRowTableData($an, $luna, $nr, $joc_complet) . PHP_EOL;
     }
     return $randuri;
 }
 
+// FIXME: stringul rezultatul rupe tabelul
 function getRowTableData(...$cols) {
     $tableRow= "<tr>";
     foreach ($cols as $col) {
-        $tableRow .= "<td>$col</td>";
+        $tableRow .= getTd($col);
     }
-    return $tableRow."</tr><br>".PHP_EOL;
+    return $tableRow . "</tr>";
+}
+
+function getTd($cellValue) {
+    return "<td>$cellValue</td>";
+}
+
+
+// FIXME: temp function pana o repari pe aia normala
+// CACAT, nici asta nu afiseaza tot tabelul cplm
+function afiseazaMotherFuckingTabelCaCelaltSeIntrerupe($toateEditiile) {
+    // table header
+    echo "<!DOCTYPE html><html><head><meta charset=\"utf-8\"></head><body>";
+    echo "<table><tr><th>An</th><th>Luna</th><th>Nr.</th><th>Joc full</th></tr>";
+
+    // table body
+    while ($row = $toateEditiile->fetchArray(SQLITE3_ASSOC)) {
+        $id =   isset($row['editie_id'])            ? $row['editie_id'] : "-";
+        $an =   isset($row['an'])                   ? $row['an'] : "-";
+        $luna = isset($row['luna'])                ? $row['luna'] : "-";
+        $nr =   isset($row['nr'])                  ? $row['nr'] : "-";
+        $joc_complet =   isset($row['joc_complet']) ? $row['joc_complet'] : "-";
+        echo "<tr><td>$id</td><td>$an</td><td>$luna</td><td>$nr</td><td>$joc_complet</td></tr>" . PHP_EOL;
+    }
+    echo "</table>";
+    echo "</body></html>";
 }
