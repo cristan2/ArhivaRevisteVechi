87220b77641507bf3741cc96e42808847ef87efe
cristan <13849765+cristan2@users.noreply.github.com>
2017-10-22 01:35:14 +0300

    Reviste: am uitat sa adaug fix clasa Revista; updatat si reviste.php

---
+++ lib/Revista.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace ArhivaRevisteVechi\lib;
+use ArhivaRevisteVechi\resources\db\DBC;
+use ArhivaRevisteVechi\lib\helpers\HtmlPrinter;
+
+// ROOT si config.php sunt definite de index.php
+//require_once("../resources/config.php");
+
+require_once HELPERS . "/HtmlPrinter.php";
+
+class Revista {
+
+    // base attrs
+    public  $numeRevista, $revistaId;
+    public  $maxNumEditii;
+
+    // location attrs
+    public  $revistaDirPath;
+    public  $revistaBaseName;
+
+    // content attrs
+    private $countEditiiScanate;
+
+    public function __construct($dbRow)
+    {
+        // base attrs
+        $this->numeRevista     = $dbRow[DBC::REV_NUME];
+        $this->revistaId       = $dbRow[DBC::REV_ID];
+        $this->maxNumEditii    = $this->getMaxNumAparitii($dbRow[DBC::REV_APARITII]);
+
+        // location attrs
+        $this->revistaBaseName = self::getSimpleName($this->numeRevista);
+        $this->revistaDirPath  = $this->buildRevistaBaseDir();
+
+        // content attrs
+        $this->countEditiiScanate   = $dbRow[DBC::REV_CNT_ED];
+    }
+
+    static function getSimpleName($numeRevista)
+    {
+        return preg_replace('/[^a-z0-9 ]+/', "", strtolower($numeRevista));
+    }
+
+    private function buildRevistaBaseDir()
+    {
+        return IMG . DIRECTORY_SEPARATOR
+            . $this->revistaBaseName;
+    }
+
+    private function getMaxNumAparitii($aparitiiFromDb)
+    {
+        return explode(" ", $aparitiiFromDb, 2)[0];
+    }
+
+    private function getStatusArhiva()
+    {
+        if (empty($this->countEditiiScanate)) $this->countEditiiScanate = "0";
+        return "$this->countEditiiScanate / $this->maxNumEditii";
+    }
+
+    public function getRevistaUrl()
+    {
+        return ARHIVA . "/editii.php" . "?revista=$this->revistaId";
+    }
+
+    private function makeCoperta()
+    {
+        $targetLink = $this->getRevistaUrl();
+        $copertaImagePath =  COPERTI_DIR . DIRECTORY_SEPARATOR . $this->revistaBaseName . ".jpg";
+        if (!file_exists($copertaImagePath)) $copertaImagePath = COPERTA_DEFAULT;
+        return getImageWithLink($copertaImagePath, $targetLink, "card-img");
+    }
+
+    /**
+     * Construieste carduri reviste
+     * cu 3 sectiuni: Imagine, Titlu si Subtitlu
+     */
+    function getHtmlOutput($useSearchLayout = false)
+    {
+        $htmlClass = "reviste-cards";
+
+        $attrsToPrint = array(
+            "imagine card"  => $this->makeCoperta(),
+            "titlu card"    => "<h1>{$this->numeRevista}</h1>",
+            "subtitlu card" => "<h2>{$this->getStatusArhiva()}</h2>",
+        );
+
+        return HtmlPrinter::buildCardDiv($attrsToPrint, $htmlClass);
+    }
+}
