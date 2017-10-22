b4ca4cc1ab0efa67558df4cb99230f2426296058
cristan <13849765+cristan2@users.noreply.github.com>
2017-10-22 11:41:21 +0300

    Articole: editii nav - inlocuit prev si next links cu Editii

--- arhiva/bits/articole_bit_editia_curenta.php
+++ arhiva/bits/articole_bit_editia_curenta.php
@@ -1,13 +1,15 @@
 <?php
-use ArhivaRevisteVechi\resources\db\DBC;
+use ArhivaRevisteVechi\lib\Editie;
 
-// next & prev links
+// previous Editie
+$prevEditieDbResult = $db->getNextRow(
+    $db->queryEditieFromNumar($editiaCurenta->revistaId, ($editiaCurenta->numar) - 1));
+$prevEditie = new Editie($prevEditieDbResult);
+$prevEditieLink = $prevEditie->getEditieUrl();
+
+// next Editie
 // TODO: trebuie sa existe si un max(editie_id) pentru disable la navLinkNext
-$prevEditieId = $db->getNextRow($db->queryEditieIdFromNumar($editiaCurenta->revistaId, ($editiaCurenta->numar) - 1));
-$navLinkPrev = getEditieUrl($prevEditieId[DBC::ED_ID]);
-
-$nextEditieId = $db->getNextRow($db->queryEditieIdFromNumar($editiaCurenta->revistaId, ($editiaCurenta->numar) + 1));
-$navLinkNext = getEditieUrl($nextEditieId[DBC::ED_ID]);
-
-$titluEditieCurenta = "{$editiaCurenta->numeRevista} nr. {$editiaCurenta->numar}";
-$lunaEditieCurenta = "(". convertLuna($editiaCurenta->luna) ." {$editiaCurenta->an})";
+$nextEditieDbResult = $db->getNextRow(
+    $db->queryEditieFromNumar($editiaCurenta->revistaId, ($editiaCurenta->numar) + 1));
+$nextEditie = new Editie($nextEditieDbResult);
+$nextEditieLink = $nextEditie->getEditieUrl();
--- html/bits/html_bit_header_main_nav.php
+++ html/bits/html_bit_header_main_nav.php
@@ -1,15 +1,20 @@
 <div class = "header-nav-container">
     <div class = "header-nav-elem header-nav-prev">
         <h2><?php
+//            if ($editiaCurenta->isFirst) echo "---";  // TODO implement isFirst
             if ($editieId <= 1) echo "---";
-            else echo "<a href='$navLinkPrev'>Prev</a>"; ?>
+            else echo "<a href='$prevEditieLink'>Prev</a>"; ?>
         </h2>
     </div>
     <div class = "header-nav-current">
-        <h1><?php echo $titluEditieCurenta?></h1>
-        <h2><?php echo $lunaEditieCurenta?></h2>
+        <h1><?php echo $editiaCurenta->getTitluDetaliat()?></h1>
+        <h2><?php echo $editiaCurenta->getInfoEditie()?></h2>
     </div>
     <div class = "header-nav-elem header-nav-next">
-        <h2><?php echo "<a href='$navLinkNext'>Next</a>"; ?></h2>
+        <h2><?php
+//            if ($editiaCurenta->isLast) echo "---";   // TODO implement isLast
+            if (false) echo "---";
+            else echo "<a href='$nextEditieLink'>Next</a>"; ?>
+        </h2>
     </div>
 </div>
