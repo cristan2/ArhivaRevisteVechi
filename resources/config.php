3fca7a68f621ee6b7b73bc29d5516124321715e0
cristan <13849765+cristan2@users.noreply.github.com>
2017-10-31 22:20:57 +0200

    DB: versiune noua baza de date

--- resources/config.php
+++ resources/config.php
@@ -23,7 +23,7 @@ DEFINE ("IMG"       , RESOURCES ."/img");
 
 // db
 DEFINE ("DB_DIR"    , RESOURCES . "/db");
-DEFINE ("DB_FILE"   , DB_DIR . "/arhiva_reviste_v4.4.db");
+DEFINE ("DB_FILE"   , DB_DIR . "/arhiva_reviste_v5.db");
 
 require_once DB_DIR . "/DBC.php";
 use ArhivaRevisteVechi\resources\db\DBC;
