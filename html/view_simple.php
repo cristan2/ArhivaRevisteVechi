81c0cd88e532d35574dea54339f6ad4601df88d0
cristan <13849765+cristan2@users.noreply.github.com>
2017-12-30 17:48:25 +0200

    General: adaugare statistici mythweb

--- html/view_simple.php
+++ html/view_simple.php
@@ -5,6 +5,7 @@
     <title><?php echo (!empty($currentPageTitle) ? $currentPageTitle . " &bull; " : "") . SITE_NAME ?></title>
     <link rel="stylesheet" href="<?php echo CSSLIB.'/fonts.css'; ?>" >
     <link rel="stylesheet" href="<?php echo CSSLIB.'/stylesheet_basic.css'; ?>" >
+    <!--link rel="icon" type = "image/png" href="<?php /*echo HTMLLIB.'/favicon/reviste-vechi.png';*/ ?>" -->
 </head>
 
 <body>
@@ -21,5 +22,8 @@
     <!--footer>
         <?php /*include HTMLBITS . "/html_bit_footer.php";*/ ?>
     </footer-->
+
+    <!-- statistics for mythweb.ro (de sters/modificat pentru alte domenii -->
+    <script type='text/javascript' src='http://mythweb.ro/fstats/js/fs2.js.php?site_id=1'></script>
 </body>
 </html>
